e4f77b1d1f32d9d893aa07692c788f049370ecb5
Gary Benson <gary@gbenson.net>
2026-06-14 21:18:12 +0100

    Signal.Mul

--- signal.go
+++ signal.go
@@ -1,7 +1,13 @@
 package picosynth
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
+// Signal is a signed Q1.31 fixed point number with range [-1,1).
+//
+// See https://en.wikipedia.org/wiki/Q_(number_format).
 type Signal int32
 
 const (
@@ -9,3 +15,18 @@ const (
 	MinSignal  = math.MinInt32
 	MaxSignal  = math.MaxInt32
 )
+
+// Mul returns the signed Q1.31 product of a and b.
+//
+// Note that MinSignal.Mul(MinSignal) overflows, returning MinSignal
+// (-1) instead of the closer-to-correct MaxSignal (1).  This may or
+// may not be corrected in future, so don't rely on it, but the point
+// of using integer math over floating point here is for performance,
+// not accuracy.
+func (a Signal) Mul(b Signal) Signal {
+	return Signal((int64(a) * int64(b)) >> 31)
+}
+
+func (v Signal) String() string {
+	return fmt.Sprintf("0x%08x", uint32(v))
+}
---
+++ signal_test.go
@@ -0,0 +1,55 @@
+package picosynth
+
+import "testing"
+
+func TestSignalMul(t *testing.T) {
+	for _, tc := range []struct {
+		a, b, want Signal
+	}{
+		{0, 0, 0},
+
+		{0, MaxSignal, 0},
+		{MaxSignal, 0, 0},
+		{MaxSignal, MaxSignal, MaxSignal},
+
+		{0, MinSignal, 0},
+		{MinSignal, 0, 0},
+		{MinSignal, MaxSignal, MinSignal},
+
+		{MaxSignal, MinSignal, MinSignal},
+		{MinSignal, MinSignal, MaxSignal},
+
+		{1234567890, MaxSignal, 1234567890},
+		{MaxSignal, 987654321, 987654321},
+
+		{1234567890, MinSignal, -1234567890},
+		{MinSignal, 987654321, -987654321},
+
+		{-1234567890, MaxSignal, -1234567890},
+		{MaxSignal, -987654321, -987654321},
+
+		{-1234567890, MinSignal, 1234567890},
+		{MinSignal, -987654321, 987654321},
+	} {
+		got := tc.a.Mul(tc.b)
+		var wanted string
+		if got != tc.want {
+			if tc.want > 0 && got > 0 && got == tc.want-1 {
+				// both positive, got off by one towards zero.
+				wanted = "*"
+			} else if tc.want < 0 && got < 0 && got == tc.want+1 {
+				// both negative, got off by one towards zero.
+				wanted = "*"
+			} else {
+				wanted = "!= " + tc.want.String()
+				if tc.a == MinSignal && tc.b == MinSignal && got == MinSignal {
+					wanted += " expected failure" // XXX do we care?
+				} else {
+					t.Fail()
+				}
+			}
+		}
+		t.Log(tc.a, "×", tc.b, "=", got, wanted)
+		//assert.Equal(t, got, tc.want)
+	}
+}
